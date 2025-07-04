a8b93b173d3fcee182cd3b41fe84c5f2dc1d143b
leen-neel <and24903@gmail.com>
2025-07-05 00:25:31 +0530

    fix

--- zen.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyBX3f4q0AyPOoVW4b4SS6qasThyr8PvcYc",
-  projectName: "test",
-  author: "Afrin",
-  include: ["components", "pages", "api"],
-  outputDir: "docs",
-  useTranslation: true,
-  languages: ["en", "fr"],
-};
